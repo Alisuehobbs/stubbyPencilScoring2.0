ba951784216e77b683ee266fc7d0bf32cdfc90bc
Kristin Sztengel <kcsztengel@gmail.com>
2016-11-22 12:19:49 -0700

    client side setup

---
+++ client/js/app.js
@@ -0,0 +1,33 @@
+var app = angular.module('stubby', ['ngRource', 'ngAnimate', 'ngCookies'])
+
+app.config(function($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: '../views/main.html',
+            controller: 'UserController'
+        })
+        .when('/signup', {
+            templateUrl: '../views/signup.html',
+            controller: 'UserController'
+        })
+        .when('/login', {
+            templateUrl: '../views/login.html',
+            controller: 'UserController'
+        })
+        .when('/profile:id', {
+          templateUrl: '../views/oneprofile.html',
+          controller: 'ProfileController'
+        })
+        .when('/creategame', {
+          templateUrl: '../views/creategame.html',
+          controller: 'GameController'
+        })
+        .when('/scorecard:id', {
+          templateUrl: '../views/scorecard.html',
+          controller: 'ScoreController'
+        })
+
+});
+
+
+//CalculatorController...  RoundsController
